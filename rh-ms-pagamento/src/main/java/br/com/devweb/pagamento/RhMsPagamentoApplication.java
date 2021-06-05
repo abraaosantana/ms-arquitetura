f6a8a5278898e25681d6424c92098e70289cdd02
Abraão Santana <deploys.app@gmail.com>
2021-06-05 19:50:50 -0300

    Microserviço: rh-ms-pagamento

---
+++ rh-ms-pagamento/src/main/java/br/com/devweb/pagamento/RhMsPagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.devweb.pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhMsPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhMsPagamentoApplication.class, args);
+	}
+
+}
---
+++ rh-ms-pagamento/src/test/java/br/com/devweb/pagamento/RhMsPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devweb.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhMsPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
