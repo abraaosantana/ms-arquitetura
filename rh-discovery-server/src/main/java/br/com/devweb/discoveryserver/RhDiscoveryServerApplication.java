212c99bf764b19d0686ca8e144c2389a527808af
Abraão Santana <deploys.app@gmail.com>
2021-07-23 18:42:19 -0300

    Discovery server

---
+++ rh-discovery-server/src/main/java/br/com/devweb/discoveryserver/RhDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.devweb.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ rh-discovery-server/src/test/java/br/com/devweb/discoveryserver/RhDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devweb.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
