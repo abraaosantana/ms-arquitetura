096fdedcbef630bfbdde3a9d10e296a18be2757f
Abraão Santana <deploys.app@gmail.com>
2021-07-25 14:10:52 -0300

    Criação Servidor de Configuração

---
+++ rh-config-server/src/test/java/br/com/devweb/configserver/RhConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devweb.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
