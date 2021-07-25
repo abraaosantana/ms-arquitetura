0d5da02b620cda9136ae39c55af3209f8a98ce7e
Abraão Santana <deploys.app@gmail.com>
2021-07-25 10:37:26 -0300

    Criação Api Gateway

---
+++ rh-api-gateway/src/test/java/br/com/devweb/apigateway/RhApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devweb.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
