b4ec00646c1f2e633b3b3db42ca17b40c5dafd0c
Abraão Santana <deploys.app@gmail.com>
2021-07-26 20:18:18 -0300

    Projeto Cadastro de Usuários

---
+++ rh-ms-user/src/main/java/br/com/devweb/rhuser/RhMsUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.devweb.rhuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhMsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhMsUserApplication.class, args);
+	}
+
+}
---
+++ rh-ms-user/src/test/java/br/com/devweb/rhuser/RhMsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devweb.rhuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhMsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
