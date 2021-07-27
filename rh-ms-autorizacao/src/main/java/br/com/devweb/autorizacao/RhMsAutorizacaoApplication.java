762de4c229cc8ca2b80805a139ad17c7e5e918d8
Abraão Santana <deploys.app@gmail.com>
2021-07-27 16:37:55 -0300

    Criação Projeto Autorização

---
+++ rh-ms-autorizacao/src/main/java/br/com/devweb/autorizacao/RhMsAutorizacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.devweb.autorizacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhMsAutorizacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhMsAutorizacaoApplication.class, args);
+	}
+
+}
---
+++ rh-ms-autorizacao/src/test/java/br/com/devweb/autorizacao/RhMsAutorizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devweb.autorizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhMsAutorizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
